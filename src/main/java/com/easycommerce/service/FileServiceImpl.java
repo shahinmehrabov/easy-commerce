abe5465547b04ec02a2ef50fb6f3bec142ae04f4
Shahin <sahinmehrabov@gmail.com>
2024-09-11 02:21:20 +0200

    feat: add function to upload image to images folder and return name

---
+++ src/main/java/com/easycommerce/service/FileServiceImpl.java
@@ -0,0 +1,30 @@
+package com.easycommerce.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    @Override
+    public String uploadImage(String path, MultipartFile image) throws IOException {
+        String originalImageName = image.getOriginalFilename();
+        String randomId = UUID.randomUUID().toString();
+        String imageName = randomId.concat(originalImageName.substring(originalImageName.lastIndexOf('.')));
+        String imagePath = path + File.pathSeparator + imageName;
+
+        File folder = new File(path);
+        if (!folder.exists())
+            folder.mkdir();
+
+        Files.copy(image.getInputStream(), Paths.get(imagePath));
+
+        return imageName;
+    }
+}

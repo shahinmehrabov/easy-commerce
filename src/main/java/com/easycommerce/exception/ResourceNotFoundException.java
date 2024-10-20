36c096bf252c589b489f2a9d7dd5347ecc616240
Shahin <sahinmehrabov@gmail.com>
2024-10-20 22:08:12 +0200

    refactor: exception package

--- src/main/java/com/easycommerce/exception/ResourceNotFoundException.java
+++ src/main/java/com/easycommerce/exception/ResourceNotFoundException.java
@@ -2,6 +2,10 @@ package com.easycommerce.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
 
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
     public ResourceNotFoundException(String resourceName, String field, String fieldName) {
         super(String.format("%s not found with %s: %s", resourceName, field, fieldName));
     }

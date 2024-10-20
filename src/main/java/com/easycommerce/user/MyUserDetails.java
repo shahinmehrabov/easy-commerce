34f541ad5ec62e5652814934335bf8751d0386e4
Shahin <sahinmehrabov@gmail.com>
2024-10-20 19:58:11 +0200

    fix: authorities return method

--- src/main/java/com/easycommerce/user/MyUserDetails.java
+++ src/main/java/com/easycommerce/user/MyUserDetails.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -33,7 +32,7 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return authorities;
     }
 
     @Override
